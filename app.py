225ca382b9752cd6170ab6d65015cf4a7eea2423
Yash Raval <yashraval05@gmail.com>
2022-05-29 15:17:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 model = pickle.load(open('random_forest_regression_model.pkl', 'rb'))
 @app.route('/',methods=['GET'])
 def Home():
-    return render_template('index.html')
+    return render_template('templates/index.html')
 
 
 standard_to = StandardScaler()
